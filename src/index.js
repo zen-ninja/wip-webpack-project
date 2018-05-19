9b398ad9dcc488202acd401ceaa9ad5f62bb4508
Zen-Ninja <ninja@zen-ninja.co.za>
2018-05-19 11:26:34 +0200

    asset management
    load css and images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+/* JS modules */
 import _ from 'lodash';
+/* CSS Modules */
+import './style.css';
+/* Images to be utilized for dynamic methods (eg: to swap out images)*/
+import Icon from './img/icons/icon.png';
+/* * */
 function component() {
   var element = document.createElement('div');
-
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  //test add image to our div
+  var myRandomIcon = new Image();
+  myRandomIcon.src = Icon;
+  element.appendChild(myRandomIcon);
 
   return element;
 }
