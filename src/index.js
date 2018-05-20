957b15fc06b6be8310a89b95c8df6dba44451dc2
Zen-Ninja <ninja@zen-ninja.co.za>
2018-05-20 11:30:41 +0200

    Progress
    Website has some actual context: styling and html in place. Customisation can now occur

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 /* CSS Modules */
 import './style.css';
 /* Images to be utilized for dynamic methods (eg: to swap out images)*/
-import Icon from './img/icons/icon.png';
+// import Icon from './img/icons/icon.png';
 /* Data files (if no db connected) */
 import StaticData from './data/test-data.xml';
 /* * */
@@ -14,8 +14,8 @@ function component() {
 
   // test add image to our div
   var myRandomIcon = new Image();
-  myRandomIcon.src = Icon;
-  element.appendChild(myRandomIcon);
+  // myRandomIcon.src = Icon;
+  // element.appendChild(myRandomIcon);
 
   // log out static data
   console.log(StaticData);
